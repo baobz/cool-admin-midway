d52271a08ef065525d7dec0cdd0d1b78258be818
baobz <baobz@sina.cn>
2021-05-12 21:09:25 +0800

    本地数据库修改

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -9,8 +9,8 @@ export default (appInfo: EggAppInfo) => {
     type: 'mysql',
     host: '127.0.0.1',
     port: 3306,
-    username: 'root',
-    password: '123123',
+    username: 'midway',
+    password: '123456',
     database: 'cool',
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: true,
